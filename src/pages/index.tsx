fd2ecf5fd9cdb4a5f85fb36919299f18d3e34058
Michele Memoli <michele@100shapes.com>
2020-03-14 13:35:47 +0000

    Fixed build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,36 +2,14 @@ import React from 'react';
 import { Heading } from '@chakra-ui/core';
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
-import { ChildImageSharp } from '../types';
 
-type PageProps = {
-  data: {
-    firstProject: {
-      title: string;
-      slug: string;
-      cover: ChildImageSharp;
-    };
-    threeProjects: {
-      nodes: {
-        title: string;
-        slug: string;
-        cover: ChildImageSharp;
-      }[];
-    };
-    aboutUs: ChildImageSharp;
-    instagram: ChildImageSharp;
-  };
-};
+type PageProps = {};
 
-const Index: React.FunctionComponent<PageProps> = ({
-  data: { firstProject, threeProjects, aboutUs, instagram }
-}) => {
-  return (
-    <Layout>
-      <SEO />
-      <Heading>Hi</Heading>
-    </Layout>
-  );
-};
+const Index: React.FunctionComponent<PageProps> = () => (
+  <Layout>
+    <SEO />
+    <Heading>Hi</Heading>
+  </Layout>
+);
 
 export default Index;
