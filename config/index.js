6254aa45931027a6d7f72ae1454a4add4435aaef
Ponk Sookatup <ponk@architracker.co>
2020-03-22 14:38:39 +0000

    Change name

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Contact Tracking', // Navigation and Site Title
-  siteTitleAlt: 'Contact Tracking - Coronavirus', // Alternative Site title for SEO
-  siteTitleShort: 'Contact Tracking', // short_name for manifest
+  siteTitle: 'Contact Tracing', // Navigation and Site Title
+  siteTitleAlt: 'Contact Tracing - COVID-19', // Alternative Site title for SEO
+  siteTitleShort: 'Contact Tracing', // short_name for manifest
   siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://contact-tracking.app', // Domain of your site. No trailing slash!
+  siteUrl: 'https://contacttracing.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription:
