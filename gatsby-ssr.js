7ace6a0595ea8d6d16ce6c9744d84d8197ad5a67
Michele Memoli <michele@100shapes.com>
2020-03-28 13:11:34 +0000

    Added i18n

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,28 @@
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/database';
-import i18n from './src/i18n';
+import { renderToString } from 'react-dom/server';
+import Backend from 'i18next-sync-fs-backend';
+import i18n from 'i18next';
 
 // https://mobx-react.js.org/recipes-ssr
 import { useStaticRendering } from 'mobx-react-lite';
 useStaticRendering(true);
 
 export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  i18n.loadNamespaces(['common'], () => {
-    replaceBodyHTMLString(bodyComponent);
-  });
+  i18n
+    .use(Backend)
+    // load the common namespace
+    .loadNamespaces(['common'], () => {
+      replaceBodyHTMLString(renderToString(bodyComponent));
+    })
+    .init({
+      initImmediate: false,
+      backend: {
+        // when this site renders serverside we want to get the locales from the src folder
+        loadPath: 'src/locales/{{lng}}/{{ns}}.json'
+      }
+    });
 };
 
 export { wrapRootElement } from './gatsby-browser';
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,27 +1,26 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import XHR from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    // have a common namespace used around the full app
-    ns: ['common'],
-    defaultNS: 'common',
-    saveMissing: true,
-    initImmediate: false,
-    debug: true,
-    interpolation: {
-      escapeValue: false // not needed for react!!
-    },
-    react: {
-      wait: true,
-      useSuspense: false
-    }
-  });
+const options = {
+  fallbackLng: 'en',
+  // have a common namespace used around the full app
+  ns: ['common'],
+  defaultNS: 'common',
+  backend: {
+    loadPath: '/locales/{{lng}}/{{ns}}.json'
+  },
+  debug: true,
+  interpolation: {
+    escapeValue: false // not needed for react!!
+  },
+  react: {
+    wait: true,
+    useSuspense: false
+  }
+};
+
+i18n.use(XHR).use(LanguageDetector).use(initReactI18next).init(options);
 
 export default i18n;
