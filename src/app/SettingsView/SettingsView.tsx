df1ceef6d1604773f8fcd318972545f36c8e1fee
Michele Memoli <michele@100shapes.com>
2020-03-29 14:27:49 +0100

    Updating notification settings #15

--- src/app/SettingsView/SettingsView.tsx
+++ src/app/SettingsView/SettingsView.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
+import { useAuth } from 'gatsby-theme-firebase';
+import { Spinner } from '@chakra-ui/core';
 import SettingsForm from './SettingsForm';
 import PageHeader from '../../components/PageHeader';
 
-const SettingsView = () => (
-  <>
-    <PageHeader
-      heading="Settings"
-      lead="Change your preferences on Contract Tracing."
-    />
-    <SettingsForm />
-  </>
-);
+const SettingsView = () => {
+  const { isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <PageHeader
+        heading="Settings"
+        lead="Change your preferences on Contract Tracing."
+      />
+      <SettingsForm />
+    </>
+  );
+};
 
 export default SettingsView;
