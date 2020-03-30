ea849e167b65a57850b439baa2e8eb1e7601283e
Michele Memoli <michele@100shapes.com>
2020-03-30 11:43:59 +0100

    Fixed SEO rendering image

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     'Tracing contact between individuals in the fight against COVID-19', // Headline for schema.org JSONLD
   siteUrl: 'https://contacttracing.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: 'defaultShareImage.png', // Used for SEO and manifest
+  siteLogo: '/defaultShareImage.png', // Used for SEO and manifest
   siteDescription: 'Keep the ones you love informed if you fall ill.',
   author: 'ContactTracing', // Author for schema.org JSONLD
 
