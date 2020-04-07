20a17224e5368848c8c12e89147f6b9e725022a7
Cactice <14835424+Cactice@users.noreply.github.com>
2020-04-07 16:04:59 +0900

    Add useScript hooks (not async)

---
+++ src/hooks/useScript.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+const useScript = (url: string) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = url;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [url]);
+};
+
+export default useScript;
