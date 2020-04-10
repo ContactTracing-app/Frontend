d7b2f949b9b21bd52cc882d679a9ebf1382b56fd
Yogen <firacloudtech@gmail.com>
2020-04-11 03:36:21 +0800

    added password reset. This closes #42.

--- src/app/SigninView/SignInForm.tsx
+++ src/app/SigninView/SignInForm.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { Form, auth, FormState } from 'gatsby-theme-firebase';
-import { Stack, Box, Input, Text, Button, Flex } from '@chakra-ui/core';
+import {
+  Stack,
+  Box,
+  Input,
+  Text,
+  Button,
+  Flex,
+  useToast
+} from '@chakra-ui/core';
 import { Field } from 'formik';
 import { useIntl, navigate } from 'gatsby-plugin-intl';
 
@@ -15,52 +23,116 @@ const SignInForm: React.FunctionComponent<{
   const [email, setemail] = useState('');
   const [password, setpassword] = useState('');
   const { setErrorMessage, errorMessage } = FormState.useContainer();
+  const [showPasswordResetForm, setshowPasswordResetForm] = useState(false);
+  const toast = useToast();
   return (
     <Stack spacing={4}>
       <Box width={['90%', '80%', '60%', '60%']} my={'2'}>
-        <form
-          {...restProps}
-          onSubmit={async (event: any) => {
-            event.preventDefault();
-            try {
-              const { user } = await auth.signInWithEmailAndPassword(
-                email,
-                password
-              );
-              if (user) {
+        {showPasswordResetForm ? (
+          <form
+            {...restProps}
+            onSubmit={async (event: any) => {
+              event.preventDefault();
+              let result = '';
+              try {
+                await auth.sendPasswordResetEmail(email).then((data) => {
+                  toast({
+                    position: 'bottom-right',
+                    description:
+                      'Password reset link was sent. Please check your email',
+                    title: 'Reset link sent',
+
+                    status: 'success',
+                    isClosable: true
+                  });
+                  console.log(data);
+                });
+              } catch (error) {
+                setErrorMessage(error.message);
               }
+              return result;
+            }}
+          >
+            <Input
+              type="email"
+              placeholder="Email registered with Contact Tracing"
+              my={2}
+              value={email}
+              onChange={(e: any) => setemail(e.target.value)}
+            />
 
-              onSuccess(user);
-            } catch (error) {
-              setErrorMessage(error.message);
-            }
-          }}
-        >
-          <Input
-            type="email"
-            placeholder="Email"
-            my={2}
-            value={email}
-            onChange={(e: any) => setemail(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="Password"
-            my={1}
-            onChange={(e: any) => setpassword(e.target.value)}
-          />
+            <Box>
+              <Button
+                my={4}
+                variantColor="teal"
+                type="submit"
+                isDisabled={!email}
+              >
+                Send Email
+              </Button>
+              <Button
+                mx={2}
+                variant="ghost"
+                variantColor="teal"
+                onClick={() => setshowPasswordResetForm(!showPasswordResetForm)}
+              >
+                {showPasswordResetForm ? 'Login Instead' : 'Forgot Password?'}
+              </Button>
+            </Box>
+          </form>
+        ) : (
+          <form
+            {...restProps}
+            onSubmit={async (event: any) => {
+              event.preventDefault();
+              try {
+                const { user } = await auth.signInWithEmailAndPassword(
+                  email,
+                  password
+                );
+                if (user) {
+                }
 
-          <Box>
-            <Button
-              my={4}
-              variantColor="teal"
-              type="submit"
-              isDisabled={!email || !password}
-            >
-              Sign In
-            </Button>
-          </Box>
-        </form>
+                onSuccess(user);
+              } catch (error) {
+                setErrorMessage(error.message);
+              }
+            }}
+          >
+            <Input
+              type="email"
+              placeholder="Email"
+              my={2}
+              value={email}
+              onChange={(e: any) => setemail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              my={1}
+              onChange={(e: any) => setpassword(e.target.value)}
+            />
+
+            <Box>
+              <Button
+                my={4}
+                variantColor="teal"
+                type="submit"
+                isDisabled={!email || !password}
+              >
+                Sign In
+              </Button>
+              <Button
+                variant="ghost"
+                mx={2}
+                variantColor="teal"
+                onClick={() => setshowPasswordResetForm(!showPasswordResetForm)}
+              >
+                {showPasswordResetForm ? 'Login Instead' : 'Forgot Password?'}
+              </Button>
+            </Box>
+          </form>
+        )}
 
         {errorMessage && <Text color="red.500">{errorMessage} </Text>}
       </Box>
